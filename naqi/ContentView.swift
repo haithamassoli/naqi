be6b58e1a667ca8ceee480e317e92d8e4ce47fa4
Haitham Assoli <haitham.assoli@goldentik.com>
2026-08-04 12:11:23 +0300

    Initial Commit

---
+++ naqi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  naqi
+//
+//  Created by Goldentik on 04/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ naqi/naqiApp.swift
@@ -0,0 +1,17 @@
+//
+//  naqiApp.swift
+//  naqi
+//
+//  Created by Goldentik on 04/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct naqiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
